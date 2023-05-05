c819bae949e48b532b35ddaa30eae56e4d4d1218
Admin <Admin@BEST-KOMP>
2023-05-05 16:39:16 +0300

    Добавьте файлы проекта.

---
+++ ntcn/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных с этой сборкой.
+[assembly: AssemblyTitle("ntcn")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ntcn")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения false в параметре ComVisible делает типы в этой сборке невидимыми 
+// для компонентов COM. Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение true для требуемого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для ID библиотеки типов typelib, если этот проект видим для COM
+[assembly: Guid("0ef48642-8284-4c3e-b5d9-5f0434c4aea6")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      основной номер версии;
+//      Дополнительный номер версии 
+//      номер сборки;
+//      редакция.
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию, 
+// используя "*", как показано ниже:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
